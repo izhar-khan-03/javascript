5144e42bd0d095bfe78dce35bbc0d06a1196cd68
izhar-khan-03 <khanizhar91677@gmail.com>
2025-07-02 09:00:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.operations.js
@@ -0,0 +1,32 @@
+// let score = false
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+// // "33" => 33
+// // "33kjhsd" => NaN  NOT A NUMBER but type of NaN is Number only
+// // true => 1; false => 0
+
+
+// let isLoggedIn = 1
+
+// let booleanLoogedIn = Boolean(isLoggedIn)
+
+// console.log(typeof (booleanLoogedIn))
+// console.log(booleanLoogedIn)
+
+// // 1 => true; 0 => false
+// // ""(empty) => false
+// // "Izhar" => true
+
+let someNumber = 654
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
