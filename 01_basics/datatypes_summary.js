0c50c3c725e25edd8e267d17bed6cb74d2030e6b
izhar-khan-03 <khanizhar91677@gmail.com>
2025-08-03 12:33:24 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // let userEmail=undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id===anotherId);
+
+const bigNumber = 45645646543534154n
+
+// ---------------------------------
+
+// Reference (Non-primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let myObj={
+    name: "Izhar",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello world"); 
+    
+}
+
+console.log(typeof id);
