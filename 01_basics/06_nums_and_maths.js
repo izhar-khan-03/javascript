7eba4498e6100461fc43c24079f8a04d6d256189
izhar-khan-03 <khanizhar91677@gmail.com>
2025-08-08 13:06:41 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// +++++   MATHS  +++++++
+
+// console.log(Math)
+
+// console.log(Math.abs(-5))   // It doesnt change the value of positive number and ABSOLUTE VALUE OF -4 IS SAME AS ABS OF 4
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(4.3,64,41,362,6562,1))
+// console.log(Math.max(4.3,64,41,362,6562,1))
+
+console.log(Math.random())
+// let i = Math.floor(Math.random()*10)
+// console.log(i);
+// console.log((i)*10);
+// console.log(((i)*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+min = 10
+max = 20
+
+console.log(Math.floor(((Math.random() * 10) + 1 + min)));
+
+
+console.log(Math.floor((Math.random() * (max - min +1) + min)))
