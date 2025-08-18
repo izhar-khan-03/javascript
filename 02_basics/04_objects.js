d306e6f3c4ae23627101d9abf6ccf9a3813f612e
izhar-khan-03 <khanizhar91677@gmail.com>
2025-08-18 12:20:25 +0000

    objects part 2 (video #17)

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,58 @@
+// const tinderUser = new Object()   // Singleton Object 
+const tinderUser = {}   // Non-singleton Object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "izhar",
+            lastname: "khan"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)   // place an empty object to get a guaranted new object to get all the data from other objects other without an empty braces, all the values will be copied to the first object
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+console.log(obj3);
+
+const Users = [
+    {
+        id: 1,
+        email: "i@gmail.com",
+        rollno: 231422
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+Users[1].email
+
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
