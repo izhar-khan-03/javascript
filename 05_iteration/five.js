a5e9b85c8a9ee0dec111a8f713f341b3858f0cd5
Izhar <khanizhar91677@gmail.com>
2025-09-11 20:45:48 +0530

    video #29 11-sep-2025 30:00 watched

---
+++ 05_iteration/five.js
@@ -0,0 +1,23 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (item){
+    
+// } )
+
+// // coding.forEach( function (item){
+// //    console.log(item);
+// // } )
+
+coding.forEach( (val) => {
+    // console.log(val);
+} )
+
+function printMe(item){
+    console.log(item);
+}
+
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr)=> {
+    console.log(item, index, arr);
+} )
---
+++ 05_iteration/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+const programming = ["js", "cpp", "java", "tsx", "go", "ruby", "py"]
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
